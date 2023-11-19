b0d0fe637729ebeaa748a8ab59fd186a23999b96
wppeprpy <107760987+wppeprpy@users.noreply.github.com>
2023-11-19 22:16:46 +0800

    Add files via upload

---
+++ todoControl.php
@@ -0,0 +1,26 @@
+<?php
+require('todoModel.php');
+
+$act=$_REQUEST['act'];
+switch ($act) {
+case "listJob":
+  $jobs=getJobList();
+  echo json_encode($jobs);
+  return;  
+case "addJob":
+	
+	$jsonStr = $_POST['dat'];
+	$job = json_decode($jsonStr);
+	//should verify first
+	addJob($job->jobName,$job->jobUrgent,$job->jobContent,$job->id);
+	return;
+case "delJob":
+	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
+	//verify
+	delJob($id);
+	return;
+default:
+  
+}
+
+?>
